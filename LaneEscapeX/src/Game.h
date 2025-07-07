14bd219e892805a6cce78d42d1a9b1e98aa6c98a
carnet00083825 <00083825@Uca.edu.sv>
2025-07-06 21:29:30 -0600

    Delete LaneEscapeX directory

--- LaneEscapeX/src/Game.h
+++
@@ -1,8 +0,0 @@
-#ifndef GAME_H
-#define GAME_H
-#pragma once
-
-void runGame();
-
-#endif
-
--- LaneEscapeX/src/Utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef UTILS_H
-#define UTILS_H
-
-#include <windows.h>
-
-void gotoxy(int x, int y);
-void ocultarCursor();
-
-#endif
