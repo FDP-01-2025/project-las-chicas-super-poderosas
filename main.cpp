0d5ad6abc4f41697e9e32547429a8e2475b02589
carnet00083825 <00083825@Uca.edu.sv>
2025-07-07 17:44:06 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include "src/Game.h"
 
 int main() {
-    runGame();
-    return 0;
+    runGame();   // in the folder: g++ main.cpp src/Game.cpp src/Obstacle.cpp src/Utils.cpp -o game.exe
+    return 0;     // .\game.exe  
 }
